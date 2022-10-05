6027c4d8559c153e1549f089f078825d761f23d7
Kerry <kerryhokanson@gmail.com>
2022-10-05 05:01:56 -0500

    document creation

---
+++ quiz.js
@@ -0,0 +1,60 @@
+var quizData = [
+    {
+        question: DOM stands for ,
+        answer1:data ommitted memory,
+        answer2:dancing on Mars,
+        answer3:doing only me,
+        answer4:document object model,
+
+    },
+    {
+        question: JSON stands for ,
+        answer1:javascript object notation,
+        answer2:join string only null,
+        answer3:justify string on node,
+        answer4:joyfully singing overt nonsense,
+        correct: answer1
+    },
+    {
+        question:javascript identifies html tags by which properties,
+        answer1: id and div,
+        answer2:operator and class,
+        answer3:class and id,
+        answer4:class and pseudoclass,
+    },
+    {
+        question:in:{var element = document.getElementById("element-name");} var 
+        answer1:
+        answer2:
+        answer3:
+        answer4:
+    },
+    {
+        question:in:{var element = document.getElementById("element-name");} element
+        answer1:
+        answer2:
+        answer3:
+        answer4:
+    },
+    {
+        question:in:{var element = document.getElementById("element-name");} document
+        answer1:
+        answer2:
+        answer3:
+        answer4:
+    },
+    {
+        question:in:{var element = document.getElementById("element-name");} .getElementById
+        answer1:
+        answer2:
+        answer3:
+        answer4:
+    },
+    {
+        question:in:{var element = document.getElementById("element-name");} "element-name"
+        answer1:
+        answer2:
+        answer3:
+        answer4:
+    },
+]
